abf93d2ee85344985de7988ac6c4e8efbd9e63cb
Mohamed Kashif <c_mohamed.kashif@smartdubai.ae>
2020-09-14 15:55:56 +0400

    intial commit

--- src/main/java/com/iphrends/drools/config/DroolsConfig.java
+++ src/main/java/com/iphrends/drools/config/DroolsConfig.java
@@ -1,9 +1,11 @@
 package com.iphrends.drools.config;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.kie.api.KieServices;
-import org.kie.api.builder.*;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieRepository;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,29 +13,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.function.Consumer;
 
-import static java.util.Arrays.stream;
-import static org.apache.commons.lang3.StringUtils.substringAfter;
-import static org.kie.internal.io.ResourceFactory.newClassPathResource;
+import static org.springframework.util.ResourceUtils.FILE_URL_PREFIX;
 
 @Configuration
+@Slf4j
 public class DroolsConfig {
 
-    private static final String RULES_PATH = "rules/";
+    private static final String RULES_PATH = new StringBuilder(FILE_URL_PREFIX).append("/tmp/rules/").toString();
 
     @Bean
     public KieFileSystem kieFileSystem() throws IOException {
         KieFileSystem kieFileSystem = getKieServices().newKieFileSystem();
-        for (Resource file : getRuleFiles()) {
-            String path = file.getURL().getPath();
-            kieFileSystem.write(ResourceFactory.newClassPathResource(path.substring(path.indexOf(RULES_PATH)), "UTF-8"));
-//            kieFileSystem.write(ResourceFactory.newClassPathResource(RULES_PATH + file.getFilename(), "UTF-8"));
+        Resource[] ruleFiles = getRuleFiles();
+        if (ArrayUtils.isEmpty(ruleFiles)) {
+            log.warn("no rule files present at {}", RULES_PATH);
+        }
+        for (Resource file : ruleFiles) {
+            kieFileSystem.write(ResourceFactory.newFileResource(ResourceUtils.getFile(file.getURL()).getAbsolutePath()));
         }
         return kieFileSystem;
     }
@@ -41,7 +41,6 @@ public class DroolsConfig {
     private Resource[] getRuleFiles() throws IOException {
         ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
         return resourcePatternResolver.getResources(RULES_PATH + "**/*.drl");
-//        return resourcePatternResolver.getResources("classpath*:" + RULES_PATH +"**/"+ "**/*.*");
     }
 
     @Bean
